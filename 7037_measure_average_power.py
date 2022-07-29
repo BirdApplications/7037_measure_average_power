893c2063458a451205d595452515332d99da99ea
BirdApplications <awagner@bird-technologies.com>
2022-07-28 22:50:34 -0400

    Made many of Bran's suggested changes. However, still needing clarification on some. Still incomplete

--- 7037_measure_average_power.py
+++ 7037_measure_average_power.py
@@ -6,18 +6,11 @@
 #The sequency of operations in this script follow those outlined by the block diagram in the Recommended Procedure for 
 # Pulse Sensor Measurements document which should be included in this package
 
-from logging.config import listen
+from threading import main_thread
 import time
 import socket
 
-#Creates a socket connection between the computer and ethernet bridge
-#Connects computer to the bridge's IP address via port 5025 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-IP = input('Enter the IP address of the ethernet bridge (or press ENTER to set IP address to the default): ')
-if IP == '':
-    sock.connect(('192.168.1.151', 5025)) #default IP address
-else:
-    sock.connect((IP, 5025))
+from pip import main
 
 #Encodes user's SCPI command so it can be sent to the ethernet bridge
 def sendSCPIcommand(msg):
@@ -30,21 +23,6 @@ def sendSCPIcommand(msg):
 def readResponse():
     resp = sock.recv(1024)#1024 should be a large enough byte size for this script, but can be changed if needed
     return resp.decode()
-
-#Asks the user if they would like to specify a frequency for the sensor to track or leave it in auto-tracking
-#If the exact frequency of the line is known, then it should be entered in MHz
-#Otherwise, auto-tracking should be selected
-def measureFrequency():
-    measureMode = input('Do you want to specify a frequency to track or perform auto-tracking? (freq/auto): ')
-    measureMode = measureMode.lower()
-    if measureMode == 'freq':
-        freq = input('enter frequecny in MHz: ')
-        sendSCPIcommand('SENS:FREQ ' + str(freq))
-    elif measureMode == 'auto':
-        return
-    else:
-        print('invalid input')
-        measureFrequency()
 
 #Corresponds to the *STB? query and what to do depending on the answer.
 #*STB? reads the status byte
@@ -68,41 +46,43 @@ def readStatusByte():
         elif questionableCondition & (1 << 8):
             print('Note: Calibration not valid\n')
 
-#Tells the sensor to read and display either forward ('f') or reflected ('r') power depending on the user's request
-def readPower(powerMeas):
-    if powerMeas == 'f' or powerMeas == 'F':
-        sendSCPIcommand('FETC:FORW:AVER?')
-        answer = readResponse()
-        print(answer.strip() + ' Watts')
-    elif powerMeas == 'r' or powerMeas == 'R':
-        sendSCPIcommand('FETC:REFL:AVER?')
-        answer = readResponse()
-        print(answer.strip() + ' Watts')
+if __name__ == ‘__main__’:
+
+    #Creates a socket connection between the computer and ethernet bridge
+    #Connects computer to the bridge's IP address via port 5025 
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    IP = input('Enter the IP address of the ethernet bridge (or press ENTER to set IP address to the default): ')
+    if IP == '':
+        sock.connect(('192.168.1.151', 5025)) #default IP address
     else:
-        powerMeas = input('Invalid entry. Please enter ''f'' for forward power or ''r'' for reflected: ')
-        readPower(powerMeas)
+        sock.connect((IP, 5025))
 
+    #This is where the recommended sequence of SCPI commands begins
+    #Sequence corresponds to the Recommended Procedure for Pulse Sensor Measurements flow chart
 
-#This is where the recommended sequence of SCPI commands begins
-#Sequence corresponds to the Recommended Procedure for Pulse Sensor Measurements flow chart
-sendSCPIcommand('*RST')
+    sendSCPIcommand('*RST') #Resturn the sensor to its default configuration settings
 
-time.sleep(0.5) #wait 500ms before continuing
-
-measureFrequency()
-while True:
-    sendSCPIcommand('*CLS')
-    
     time.sleep(0.5) #wait 500ms before continuing
 
-    sendSCPIcommand('INIT') 
+    sendSCPIcommand('*CLS')
 
-    sendSCPIcommand('*OPC?')
-    readResponse() #prevents further operations until all pending operations have occurred
+    while True:
+        
+        time.sleep(0.5) #wait 500ms before continuing
 
-    readStatusByte() #handles the sequence beginning with the command *STB?
+        sendSCPIcommand('INIT') 
 
-    powerMeas = input('Read Forward Average Power or Relfected Average Power? (f/r): ')
-    if powerMeas == 'exit':
-        break
-    readPower(powerMeas)
+        sendSCPIcommand('*OPC?')
+        readResponse() #prevents further operations until all pending operations have occurred
+
+        readStatusByte() #handles the sequence beginning with the command *STB?
+
+        print('Type EXIT to end the program')
+        powerMeas = input('Press ENTER to make a measurement:')
+
+        if powerMeas.lower() == 'exit':
+            break
+        else:
+            sendSCPIcommand('FETC:AVER?')
+            answer = readResponse()
+            print(answer.strip() + ' Watts')
